efc32bc85fc8633d035df71aeb270458e900b42d
Nicholas Valbusa <squallstar@gmail.com>
2014-10-13 13:01:05 +0100

    tabs style is 4 spaces rather than 2

--- src/Images/StandardImageExtractor.php
+++ src/Images/StandardImageExtractor.php
@@ -8,16 +8,16 @@ class StandardImageExtractor extends ImageExtractor {
         $nodes = $article->getDoc()->filter('meta[property="og:image"]');
 
         if ($nodes->length) {
-          return $nodes->item(0)->getAttribute('content');
+            return $nodes->item(0)->getAttribute('content');
         }
 
         if ($article->getTopNode()) {
 
-          $nodes = $article->getTopNode()->filter('img[src]');
+            $nodes = $article->getTopNode()->filter('img[src]');
 
-          if ($nodes->length) {
-            return $nodes->item(0)->getAttribute('src');
-          }
+            if ($nodes->length) {
+                return $nodes->item(0)->getAttribute('src');
+            }
         }
     }
 }
