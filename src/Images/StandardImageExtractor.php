55a29450569c8047389dff045f7e060910c04351
Nicholas Valbusa <squallstar@gmail.com>
2014-10-14 12:44:18 +0100

    better og:image support

--- src/Images/StandardImageExtractor.php
+++ src/Images/StandardImageExtractor.php
@@ -4,11 +4,30 @@ namespace Goose\Images;
 
 class StandardImageExtractor extends ImageExtractor {
 
+    private $openGraphLogos = [
+        /* Websites such as "The Guardian" put their logo as a "og:image"
+           therefore should be used with lower priority than the image in
+           the topNode. */
+        '/icons/'
+    ];
+
     public function getBestImage($article) {
         $nodes = $article->getDoc()->filter('meta[property="og:image"]');
 
+        $img = null;
+
         if ($nodes->length) {
-            return $nodes->item(0)->getAttribute('content');
+            $img = $nodes->item(0)->getAttribute('content');
+            $valid = true;
+
+            foreach ($this->openGraphLogos as $uri) {
+                if (strpos($img, $uri) !== FALSE) {
+                    $valid = false;
+                    break;
+                }
+            }
+
+            if ($valid) return $img;
         }
 
         if ($article->getTopNode()) {
@@ -19,5 +38,7 @@ class StandardImageExtractor extends ImageExtractor {
                 return $nodes->item(0)->getAttribute('src');
             }
         }
+
+        return $img;
     }
 }
