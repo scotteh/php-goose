750eb5ed1c1603d5b1529780c66ef9c2e1707e54
Andrew <andrew@andrewscott.net.au>
2015-04-15 23:43:05 +1000

    Clean up URL parsing edge case bugs

--- src/Utils/URLHelper.php
+++ src/Utils/URLHelper.php
@@ -2,18 +2,23 @@
 
 namespace Goose\Utils;
 
+use Goose\Exceptions\MalformedURLException;
+
 class URLHelper {
     public static function getCleanedUrl($urlToCrawl) {
-        $parts = (object)parse_url($urlToCrawl);
-        $finalUrl = str_replace('#!', '?_escaped_fragment_=', $urlToCrawl);
+        $parts = parse_url($urlToCrawl);
 
         if ($parts === false) {
             throw new MalformedURLException($urlToCrawl . ' - is a malformed URL and cannot be processed');
         }
 
+        $prefix = isset($parts['query']) && $parts['query'] ? '&' : '?';
+
+        $finalUrl = str_replace('#!', $prefix . '_escaped_fragment_=', $urlToCrawl);
+
         return (object)[
             'url' => $urlToCrawl,
-            'parts' => $parts,
+            'parts' => (object)$parts,
             'linkhash' => md5($urlToCrawl),
             'finalUrl' => $finalUrl,
         ];
